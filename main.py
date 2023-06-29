56b2f889d54c48bf9da2f8c67c525f83d0e63d39
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-06-29 18:54:08 +0300

    input data visualized

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+import matplotlib.pyplot as plt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 class MainWindow(QMainWindow):
@@ -6,6 +7,21 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setGeometry(100, 100, 300, 210)
+
+        self.visualize_button = QPushButton("Visualize input data", self)
+        self.visualize_button.setGeometry(50, 25, 150, 30)
+        self.visualize_button.clicked.connect(self.visualize_input_data)
+
+    def visualize_input_data(self):
+        criteria = ['K1', 'K2', 'K3', 'K4', 'K5']
+
+        values = [0.7, 0.5, 0.5, 0.9, 0.6]
+
+        #Create a bar chart
+        plt.bar(criteria, values)
+
+        plt.show()
+
 
 def main():
     app = QApplication([])
