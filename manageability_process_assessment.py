efe7919908dbe3bbbe6f92ebdb4499f84b0b1bf4
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-06-30 17:49:50 +0300

    criterion evaluations calculated

--- manageability_process_assessment.py
+++ manageability_process_assessment.py
@@ -1,4 +1,21 @@
 class ManageabilityProcessAssessment:
 
     def evaluate_manageability_process(self, input_data, terms):
-        pass
+        T1 = [0, 20]
+        T2 = [20, 40]
+        T3 = [40, 60]
+        T4 = [60, 80]
+        T5 = [80, 100]
+
+        criterion_evaluation1 = input_data[0] * T4[1]
+
+        criterion_evaluation2 = input_data[1] * T5[1]
+
+        criterion_evaluation3 = input_data[2] * T5[1]
+
+        criterion_evaluation4 = input_data[3] * T5[1]
+
+        criterion_evaluation5 = input_data[4] * T3[1]
+
+        criterion_evaluations = [criterion_evaluation1, criterion_evaluation2, criterion_evaluation3,
+                                 criterion_evaluation4, criterion_evaluation5]
