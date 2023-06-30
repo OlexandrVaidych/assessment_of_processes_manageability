9346c22cce04f336cc574d83e99b4fe73545a442
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-06-30 17:43:49 +0300

    manageability process assessment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
+from manageability_process_assessment import ManageabilityProcessAssessment
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -18,6 +20,10 @@ class MainWindow(QMainWindow):
         self.weight_coeffs_label = QLabel("Weight coefficients: 8, 9, 9, 10, 5", self)
         self.weight_coeffs_label.setGeometry(50, 100, 200, 30)
 
+        self.assess_button = QPushButton("Assess manageability processes", self)
+        self.assess_button.setGeometry(50, 140, 200, 30)
+        self.assess_button.clicked.connect(self.assess_manageability_process)
+
 
     def visualize_input_data(self):
         criteria = ['K1', 'K2', 'K3', 'K4', 'K5']
@@ -28,6 +34,14 @@ class MainWindow(QMainWindow):
         plt.bar(criteria, values)
 
         plt.show()
+
+    def assess_manageability_process(self):
+        input_data = [0.7, 0.5, 0.5, 0.9, 0.6]
+        terms = ["T4", "T5", "T5", "T5", "T3"]
+        weight_coeffs = [8, 9, 9, 10, 5]
+
+        manageability_process_assessment = ManageabilityProcessAssessment()
+        manageability_process_assessment.evaluate_manageability_process(input_data, terms)
 
 
 def main():
---
+++ manageability_process_assessment.py
@@ -0,0 +1,4 @@
+class ManageabilityProcessAssessment:
+
+    def evaluate_manageability_process(self, input_data, terms):
+        pass
